f19624f0ebf63c1174ea93d454be4388eb8a4f39
cRazyeye19 <jlesterpansoy@gmail.com>
2025-08-11 08:44:05 +0800

    refactor(socket): migrate server to use env variables and ESM
    Update socket server to utilize environment variables for PORT and
    CORS_ORIGIN, improving configuration flexibility. This change
    involves adding the 'dotenv' package and updating the .env.example.
    
    Additionally, the server's module system has been transitioned from
    CommonJS 'require' to ES Modules 'import' syntax for consistency
    with modern JavaScript practices.

--- socket/index.js
+++ socket/index.js
@@ -1,40 +1,47 @@
-const io = require('socket.io')(8800, {
-  cors:{
-    origin: "http://localhost:3000"
-  }
-})
+import { Server } from "socket.io";
+import dotenv from "dotenv";
 
-let activeUsers = [] 
+dotenv.config();
 
-io.on("connection", (socket)=>{
-  
-  //Add new user
-  socket.on('new-user-add', (newUserId)=>{
-    //If user is not added before
-    if(!activeUsers.some((user)=> user.userId === newUserId)){
+const io = new Server(process.env.PORT || 8800, {
+  cors: {
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+  },
+});
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+  socket.on("new-user-add", (newUserId) => {
+    if (!activeUsers.some((user) => user.userId === newUserId)) {
       activeUsers.push({
-        userId: newUserId, 
-        socketId: socket.id
-      })
+        userId: newUserId,
+        socketId: socket.id,
+      });
     }
-    console.log("Connected Users", activeUsers)
-    io.emit('get-users', activeUsers)
-  })
+    console.log("Connected Users", activeUsers);
+    io.emit("get-users", activeUsers);
+  });
 
-  //Send Message
-  socket.on("send-message", (data)=>{
-    const {receiverId} = data;
-    const user = activeUsers.find((user)=> user.userId === receiverId)
-    console.log("Sending from Socket.io server to : ", receiverId)
-    console.log("Data", data)
-    if(user){
-      io.to(user.socketId).emit("receive-message", data)
+  socket.on("send-message", (data) => {
+    const { receiverId } = data;
+    const user = activeUsers.find((user) => user.userId === receiverId);
+    console.log("Sending from Socket.io server to : ", receiverId);
+    console.log("Data", data);
+    if (user) {
+      io.to(user.socketId).emit("receive-message", data);
     }
-  })
+  });
 
-  socket.on("disconnect", ()=>{
-    activeUsers = activeUsers.filter((user)=> user.socketId !== socket.id);
+  socket.on("disconnect", () => {
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
     console.log("User Disconnected", activeUsers);
-    io.emit('get-users', activeUsers)
-  })
-})
+    io.emit("get-users", activeUsers);
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+io.listen(PORT);
+console.log(`Socket server running on port ${PORT}`);
+console.log("CORS_ORIGIN:", process.env.CORS_ORIGIN);
+console.log("PORT:", process.env.PORT);
